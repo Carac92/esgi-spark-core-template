9229a1a2e2ce51c85c854ba7c415c07590e5084f
Mulet-J <71126090+Mulet-J@users.noreply.github.com>
2025-06-25 13:52:52 +0200

    add missing files and fix gitignore

--- projet_spark/build.sbt
+++ projet_spark/build.sbt
@@ -2,10 +2,12 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.12.20"
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "3.5.6" % "provided",
-  "org.apache.spark" %% "spark-sql"  % "3.5.6" % "provided"
+  "org.apache.spark" %% "spark-core" % "3.5.6",
+  "org.apache.spark" %% "spark-sql"  % "3.5.6",
+  "org.apache.hadoop" % "hadoop-client" % "3.3.4"
 )
 
+fork := true
 
 lazy val root = (project in file("."))
   .settings(
--- projet_spark/src/main/scala/Main.scala
+++ projet_spark/src/main/scala/Main.scala
@@ -1,14 +1,15 @@
 package org.esgi
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.hadoop.shaded.org.eclipse.jetty.websocket.common.frames
 
 object Main {
 
   private def readCsv(spark: SparkSession, path: String): DataFrame = {
     spark.read
-      .option("header", "true")        // Use first row as header
-      .option("inferSchema", "true")    // Automatically infer column types
-      .option("mode", "DROPMALFORMED")  // Drop malformed rows instead of failing
+      .option("header", "true") // Use first row as header
+      .option("inferSchema", "true") // Automatically infer column types
+      .option("mode", "DROPMALFORMED") // Drop malformed rows instead of failing
       .csv(path)
   }
 
@@ -17,22 +18,26 @@ object Main {
     val defaultCsv = "data/StudentPerformanceFactors.csv"
 
     // Determine paths from CLI or fallback
-    val csvPath   = args.headOption.getOrElse(defaultCsv)
-    val maybeOut  = if (args.length > 1) Some(args(1)) else None
+    val csvPath = args.headOption.getOrElse(defaultCsv)
+    val maybeOut = if (args.length > 1) Some(args(1)) else None
 
     // Boilerplate SparkSession creation; master/driver memory come from spark‑submit
     val spark = SparkSession
       .builder()
       .appName("CsvToSpark")
+      .master(
+        "local[*]"
+      ) // ← ici : exécution en local avec tous les cœurs disponibles
       .getOrCreate()
 
     try {
-      val df = readCsv(spark, csvPath)
+      var df = readCsv(spark, csvPath)
 
       // Quick inspection
       println(s"\n=== Loaded file: $csvPath ===")
       df.printSchema()
       df.show(numRows = 20, truncate = false)
+      var a = df.describe()
 
       // Optional Parquet write
       maybeOut.foreach { outPath =>
@@ -40,6 +45,7 @@ object Main {
         println(s"\nDataFrame written to $outPath in Parquet format.")
       }
     } finally {
+      while (true) {}
       spark.stop()
     }
   }
